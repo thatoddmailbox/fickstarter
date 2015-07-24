f0319b6f9ff53032e16e764b675f340feaff1cf6
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-07-24 17:52:51 +0000

    fix models

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,12 +1,4 @@
 class Donation < ActiveRecord::Base
-  include BCrypt
-
-  def password
-    @password ||= Password.new(password_hash)
-  end
-
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
-  ends
+  belongs_to :project
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20150724174316) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "bio"
-    t.string "password"
+    t.string "password_hash"
   end
 
 end
